944be117ae7bfd309a11a60b3158c8c58e7dd498
Luis <impel.up@gmail.com>
2019-01-27 13:00:43 -0600

    Se ha terminado las animaciones del menu.

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    public Animator anim_menu;
+
+
+    public string ESTADO="";
+
+	// Use this for initialization
+	void Start () {}
+
+
+    IEnumerator corrutina_SeccionTitulo() {
+        while (true) {
+            yield return new WaitForEndOfFrame();
+            if (Input.anyKey) {
+                anim_menu.Play("Menu_Ir_A_Instrucciones");
+                break;
+            }
+        }
+
+    }
+
+    IEnumerator corrutina_SeccionInstrucciones() {
+        while (true){
+            yield return new WaitForEndOfFrame();
+            if (Input.anyKey){
+                anim_menu.Play("Menu_Ir_A_Lobby");
+                break;
+            }
+        }
+    }
+
+
+    IEnumerator corrutina_SeccionLobby(){
+        while (true){
+            yield return new WaitForEndOfFrame();
+            if (Input.anyKey) {
+                anim_menu.Play("Menu_Ir_A_Comenzar");
+                break;
+            }
+        }
+    }
+
+
+    IEnumerator corrutina_SeccionComenzar() {
+        while (true) {
+            yield return new WaitForEndOfFrame();
+            if (Input.anyKey){
+                anim_menu.Play("Menu_Ir_A_Juego");
+                break;
+            }
+        }
+    }
+
+    IEnumerator corrutina_IrAJuego() {
+        while (true){
+            yield return new WaitForEndOfFrame();
+            if (Input.anyKey) {
+                anim_menu.Play("Menu_Ir_A_Juego");
+                break;
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKeyDown){
+            Debug.Log("Se ha presionado cualquier botón.");
+            CualquierTecla_Activa();
+        }
+
+    }
+
+
+    public void cambiarEstado(string nuevoEstado) {
+        Debug.Log("Se va a cambair estado = " + nuevoEstado);
+        ESTADO = nuevoEstado;
+    }
+
+
+    void CualquierTecla_Activa() {
+
+        switch (ESTADO) {
+            case "":
+            break;
+
+            case "TITULO":
+                Debug.Log("Caso titulo encontrado");
+                irAInstrucciones();
+            break;
+
+            default:
+            break;
+        }
+
+
+    }
+
+
+
+    void irAInstrucciones() {
+        
+    }
+
+
+    */
+}
