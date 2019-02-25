f0e46db3e937998136f3dd51b8ba82a9db6feafd
Luis <impel.up@gmail.com>
2019-02-24 23:26:30 -0600

    Se ha integrado el modelo del gigante al jugador HTC.

---
+++ Assets/Scripts/Golem/Golem_Cuerpo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Golem_Cuerpo : MonoBehaviour
+{
+
+    [Header("REFERENCIAS A ESCENA")]
+    public Transform posicionCamara;
+
+    [Header("CONFIGURACIONES")]
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start(){
+    }
+
+    // Update is called once per frame
+    void Update(){
+        SeguirCabeza();
+    }
+
+    private void FixedUpdate(){
+        SeguirCabeza();
+    }
+
+    public void SeguirCabeza(){
+        this.transform.position = posicionCamara.position + offset;
+
+        this.transform.rotation = Quaternion.Euler(new Vector3(0.0f, posicionCamara.transform.rotation.eulerAngles.y, 0.0f));
+     
+    }
+
+
+
+
+}
